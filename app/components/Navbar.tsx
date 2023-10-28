4e90516c59d1264086641b9b39e364e3e2760b41
Amish Verma <amishhverma@gmail.com>
2023-10-29 00:45:01 +0530

    fix navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,40 @@
-import { Container, Flex } from '@radix-ui/themes';
-import Link from 'next/link';
+'use client';
 
-export default function Navbar() {
+import classnames from 'classnames';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { AiFillBug } from 'react-icons/ai';
+
+const NavBar = () => {
+	const currentPath = usePathname();
+	
+	const links = [
+		{ label: 'Dashboard', href: '/' },
+		{ label: 'Issues', href: '/issues' },
+	];
+
 	return (
-		<nav className="border-b mb-5 px-5 py-3">
-			<Container>
-				<Flex justify="between">
-					<Flex align="center" gap="3">
-						<Link href="/">I GOT ISSUES</Link>
-					</Flex>
-				</Flex>
-			</Container>
+		<nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+			<Link href="/">
+				<AiFillBug />
+			</Link>
+			<ul className="flex space-x-6">
+				{links.map((link) => (
+					<Link
+						key={link.href}
+						className={classnames({
+							'text-zinc-900': link.href === currentPath,
+							'text-zinc-500': link.href !== currentPath,
+							'hover:text-zinc-800 transition-colors': true,
+						})}
+						href={link.href}
+					>
+						{link.label}
+					</Link>
+				))}
+			</ul>
 		</nav>
 	);
-}
+};
+
+export default NavBar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import { Button } from '@radix-ui/themes';
-
 export default function Home() {
-	return (
-		<main>
-			Hello, Issues!<Button>Click Me!</Button>
-		</main>
-	);
+	return <main>This is your Dashboard.</main>;
 }
