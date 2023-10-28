da056b4253a99ccbae4ef8d68d69191ae571176e
Amish Verma <amishhverma@gmail.com>
2023-10-28 22:31:17 +0530

    refactor root layout and tailwind config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import './globals.css';
 import '@radix-ui/themes/styles.css';
 import './theme-config.css';
-import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Theme } from '@radix-ui/themes';
@@ -8,7 +8,6 @@ import Navbar from './components/Navbar';
 
 const inter = Inter({
 	subsets: ['latin'],
-	display: 'swap',
 	variable: '--font-inter',
 });
 
