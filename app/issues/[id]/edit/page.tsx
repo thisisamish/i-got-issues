0e82e9a590e73d577e3cc7d29b387895482849b4
Amish Verma <amishhverma@gmail.com>
2023-10-28 23:51:26 +0530

    build edit issue page

---
+++ app/issues/[id]/edit/page.tsx
@@ -0,0 +1,19 @@
+import prisma from '@/prisma/client';
+import IssueForm from '../../_components/IssueForm';
+import { notFound } from 'next/navigation';
+
+type Props = {
+	params: { id: string };
+};
+
+export default async function EditIssuePage({ params }: Props) {
+	const issue = await prisma.issue.findUnique({
+		where: {
+			id: parseInt(params.id),
+		},
+	});
+
+	if (!issue) notFound();
+
+	return <IssueForm issue={issue} />;
+}
