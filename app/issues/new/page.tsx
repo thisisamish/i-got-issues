003ccfe95817c2801585bc8f97f1fc8d7a4abdbc
Amish Verma <amishhverma@gmail.com>
2023-10-28 19:38:41 +0530

    add handle form submission for new issues

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,16 +2,39 @@
 
 import { TextField, Button } from '@radix-ui/themes';
 import SimpleMDE from 'react-simplemde-editor';
+import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 import 'easymde/dist/easymde.min.css';
 
+type IssueForm = {
+	title: string;
+	description: string;
+};
+
 export default function NewIssuePage() {
+	const router = useRouter();
+	const { register, control, handleSubmit } = useForm<IssueForm>();
+
 	return (
-		<div className="max-w-xl space-y-3">
+		<form
+			onSubmit={handleSubmit(async (data) => {
+				await axios.post('/api/issues', data);
+				router.push('/issues');
+			})}
+			className="max-w-xl space-y-3"
+		>
 			<TextField.Root>
-				<TextField.Input placeholder="Title"></TextField.Input>
+				<TextField.Input placeholder="Title" {...register('title')} />
 			</TextField.Root>
-			<SimpleMDE />
+			<Controller
+				name="description"
+				control={control}
+				render={({ field }) => (
+					<SimpleMDE placeholder="Description" {...field} />
+				)}
+			/>
 			<Button>Submit New Issue</Button>
-		</div>
+		</form>
 	);
 }
