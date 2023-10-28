2b839487b8cbd64db067bcceec6fbc2507053bf0
Amish Verma <amishhverma@gmail.com>
2023-10-28 22:04:06 +0530

    add loading skeletons

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,10 +1,10 @@
-import prisma from '@/prisma/client';
 import { Table } from '@radix-ui/themes';
-import IssueStatusBadge from '../components/IssueStatusBadge';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 import IssueActions from '../components/IssueActions';
 
-export default async function IssuesPage() {
-	const issues = await prisma.issue.findMany();
+const LoadingIssuesPage = () => {
+	const issues = [1, 2, 3, 4, 5]; // just to render 5 rows of skeletons
 
 	return (
 		<div>
@@ -23,18 +23,18 @@ export default async function IssuesPage() {
 				</Table.Header>
 				<Table.Body>
 					{issues.map((issue) => (
-						<Table.Row key={issue.id}>
+						<Table.Row key={issue}>
 							<Table.Cell>
-								{issue.title}
+								<Skeleton />
 								<div className="block md:hidden">
-									<IssueStatusBadge status={issue.status} />
+									<Skeleton />
 								</div>
 							</Table.Cell>
 							<Table.Cell className="hidden md:table-cell">
-								<IssueStatusBadge status={issue.status} />
+								<Skeleton />
 							</Table.Cell>
 							<Table.Cell className="hidden md:table-cell">
-								{issue.createdAt.toDateString()}
+								<Skeleton />
 							</Table.Cell>
 						</Table.Row>
 					))}
@@ -42,4 +42,6 @@ export default async function IssuesPage() {
 			</Table.Root>
 		</div>
 	);
-}
+};
+
+export default LoadingIssuesPage;
