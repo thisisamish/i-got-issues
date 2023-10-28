1fa3ed65447140875d32fcf7475d96d8c523edbc
Amish Verma <amishhverma@gmail.com>
2023-10-28 10:29:42 +0530

    add and setup radixui

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@radix-ui/themes';
+
 export default function Home() {
-	return <main>Hello, Issues!</main>;
+	return (
+		<main>
+			Hello, Issues!<Button>Click Me!</Button>
+		</main>
+	);
 }
