35421acd1cc286b9f90b40171f30b8e8f5497d40
Amish Verma <amishhverma@gmail.com>
2023-10-29 07:57:34 +0530

    enhance api to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/prisma/client';
-import { issueSchema } from '@/app/lib/validationSchemas';
+import { patchIssueSchema } from '@/app/lib/validationSchemas';
 import authOptions from '@/app/auth/authOptions';
 import { getServerSession } from 'next-auth';
 
@@ -13,10 +13,25 @@ export async function PATCH(
 	if (!session) return NextResponse.json({}, { status: 401 });
 
 	const body = await request.json();
-	const validation = issueSchema.safeParse(body);
+	const validation = patchIssueSchema.safeParse(body);
 
 	if (!validation.success)
 		return NextResponse.json(validation.error.format(), { status: 400 });
+
+	const { assignedToUserId, title, description } = body;
+	if (assignedToUserId) {
+		const user = await prisma.user.findUnique({
+			where: {
+				id: assignedToUserId,
+			},
+		});
+
+		if (!user)
+			return NextResponse.json(
+				{ error: 'Invalid user.' },
+				{ status: 400 }
+			);
+	}
 
 	const issue = await prisma.issue.findUnique({
 		where: {
@@ -32,8 +47,9 @@ export async function PATCH(
 			id: issue.id,
 		},
 		data: {
-			title: body.title,
-			description: body.description,
+			title,
+			description,
+			assignedToUserId,
 		},
 	});
 
