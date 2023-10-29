38bcec572f673dc541e8a21f96fcd85d85a3a723
Amish Verma <amishhverma@gmail.com>
2023-10-29 13:48:16 +0530

    fix filtering bugs

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -53,7 +53,10 @@ export default async function IssuesPage({
 				<Table.Header>
 					<Table.Row>
 						{columns.map((column) => (
-							<Table.ColumnHeaderCell key={column.value}>
+							<Table.ColumnHeaderCell
+								key={column.value}
+								className={column.className}
+							>
 								<Link
 									href={{
 										query: {
