982baa1204487375b0af422f6d8899c1cf5701c9
Amish Verma <amishhverma@gmail.com>
2025-02-21 12:22:53 +0530

    fixed: can't use dynamic in server components

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,13 @@
-import dynamic from 'next/dynamic';
-import IssueFormSkeleton from './loading';
+"use client";
 
-const IssueForm = dynamic(() => import('@/app/issues/_components/IssueForm'), {
-	ssr: false,
-	loading: () => <IssueFormSkeleton />,
+import dynamic from "next/dynamic";
+import IssueFormSkeleton from "./loading";
+
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
 });
 
 export default function NewIssuePage() {
-	return <IssueForm />;
+  return <IssueForm />;
 }
