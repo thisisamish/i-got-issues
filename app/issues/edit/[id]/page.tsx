ebfa6bc2ac02f0036dbb36498ffcdee45fa70136
Amish Verma <amishhverma@gmail.com>
2025-02-21 12:26:03 +0530

    fixed: can't use dynamic in server components

--- app/issues/edit/[id]/page.tsx
+++ app/issues/edit/[id]/page.tsx
@@ -1,26 +1,28 @@
-import prisma from '@/prisma/client';
-import dynamic from 'next/dynamic';
-import { notFound } from 'next/navigation';
-import IssueFormSkeleton from './loading';
+"use client";
 
-const IssueForm = dynamic(() => import('@/app/issues/_components/IssueForm'), {
-	ssr: false,
-	loading: () => <IssueFormSkeleton />,
+import prisma from "@/prisma/client";
+import dynamic from "next/dynamic";
+import { notFound } from "next/navigation";
+import IssueFormSkeleton from "./loading";
+
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
 });
 
 type Props = {
-	params: Promise<{ id: string }>;
+  params: Promise<{ id: string }>;
 };
 
 export default async function EditIssuePage(props: Props) {
-    const params = await props.params;
-    const issue = await prisma.issue.findUnique({
-		where: {
-			id: parseInt(params.id),
-		},
-	});
+  const params = await props.params;
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
 
-    if (!issue) notFound();
+  if (!issue) notFound();
 
-    return <IssueForm issue={issue} />;
+  return <IssueForm issue={issue} />;
 }
